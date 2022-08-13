088fabea697c0cb5df40681d4a72974e6c1cfc0c
Tyler Vezina <tylerjvezina@gmail.com>
2022-08-13 09:02:09 -0400

    Added GNU GPLv3 license

--- library/cmd-emulator/cmd-emulator.js
+++ library/cmd-emulator/cmd-emulator.js
@@ -1,3 +1,20 @@
+/*
+  cmd-emulator.js - A p5.js-based emulator of the Windows command prompt
+  Copyright (C) 2022  Tyler Vezina
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 class Color {
   static Black     = 0x0;
   static DarkBlue  = 0x1;
